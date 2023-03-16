5c48d4cb4a591b8f277045534037f93e98549bba
Lucas dos Reis Viana <55816219+Lucas-vna@users.noreply.github.com>
2023-03-16 19:18:08 -0300

    Create DecobrirLista1.java

---
+++ DecobrirLista1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista1;
+
+import java.util.Random;
+
+public class Descobrir {
+
+    Padaria padaria = new Padaria();
+    Random random = new Random();
+    
+    int aleatorio = random.nextInt(padaria.qtdeFuncionarios)+1;   
+    
+}
